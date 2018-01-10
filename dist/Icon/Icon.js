6b09b71225fb6488b16bc752f5214774989c1513
Christopher Auyeung <chris@winfieldco.com>
2018-01-10 12:37:29 -0500

    removed icon.css

--- dist/Icon/Icon.js
+++ dist/Icon/Icon.js
@@ -16,8 +16,6 @@ var _classnames = require('classnames');
 
 var _classnames2 = _interopRequireDefault(_classnames);
 
-require('./Icon.css');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
